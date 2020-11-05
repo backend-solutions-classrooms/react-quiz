0c5c883344fbe673964e29a45ab0805a03ef7a50
Mehul Mohan <mehulmpt@gmail.com>
2020-11-06 01:30:17 +0530

    default files added

--- lab4.js
+++ lab4.js
@@ -1,15 +1,15 @@
 // * Production start
-// import '../code/node_modules/@testing-library/jest-dom'
-// import * as React from '../code/node_modules/react'
-// import { render, fireEvent, screen } from '../code/node_modules/@testing-library/react'
-// import App from '../code/src/App'
+import '../code/node_modules/@testing-library/jest-dom'
+import * as React from '../code/node_modules/react'
+import { render, fireEvent, screen } from '../code/node_modules/@testing-library/react'
+import App from '../code/src/App'
 // * Production end
 
 // * Development start
-import '../react-quiz/node_modules/@testing-library/jest-dom'
-import * as React from '../react-quiz/node_modules/react'
-import { render, fireEvent, screen } from '../react-quiz/node_modules/@testing-library/react'
-import App from '../react-quiz/src/App'
+// import '../react-quiz/node_modules/@testing-library/jest-dom'
+// import * as React from '../react-quiz/node_modules/react'
+// import { render, fireEvent, screen } from '../react-quiz/node_modules/@testing-library/react'
+// import App from '../react-quiz/src/App'
 // * Development end
 
 test('Question 1 is correct', () => {
@@ -24,4 +24,18 @@ test('Answers 1 are correct', () => {
 	expect(screen.getByText('London'))
 	expect(screen.getByText('Paris'))
 	expect(screen.getByText('Dublin'))
+})
+
+test('Clicking option once works fine', () => {
+	render(<App />)
+	fireEvent.click(screen.getByText('New York'))
+	expect(screen.getByText('Who is CEO of Tesla?'))
+	expect(screen.getByText('Jeff Bezos'))
+})
+
+test('Clicking option twice works fine', () => {
+	render(<App />)
+	fireEvent.click(screen.getByText('New York'))
+	fireEvent.click(screen.getByText('Jeff Bezos'))
+	expect(screen.getByText('Apple'))
 })
