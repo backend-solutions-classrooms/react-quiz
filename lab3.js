faa77954510689ab0ec26230f6710ceedc316fc5
Mehul Mohan <mehulmpt@gmail.com>
2020-11-06 01:17:01 +0530

    default files added

--- lab3.js
+++ lab3.js
@@ -1,8 +1,16 @@
-import '@testing-library/jest-dom'
-import * as React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
-// import App from '../react-quiz/src/App'
+// * Production start
+import '../code/node_modules/@testing-library/jest-dom'
+import * as React from '../code/node_modules/react'
+import { render, fireEvent, screen } from '../code/node_modules/@testing-library/react'
 import App from '../code/src/App'
+// * Production end
+
+// * Development start
+// import '../react-quiz/node_modules/@testing-library/jest-dom'
+// import * as React from '../react-quiz/node_modules/react'
+// import { render, fireEvent, screen } from '../react-quiz/node_modules/@testing-library/react'
+// import App from '../react-quiz/src/App'
+// * Development end
 
 test('Question is correct', () => {
 	const firstQuestion = 'What is the capital of France?'
---
+++ lab4.js
@@ -0,0 +1,27 @@
+// * Production start
+// import '../code/node_modules/@testing-library/jest-dom'
+// import * as React from '../code/node_modules/react'
+// import { render, fireEvent, screen } from '../code/node_modules/@testing-library/react'
+// import App from '../code/src/App'
+// * Production end
+
+// * Development start
+import '../react-quiz/node_modules/@testing-library/jest-dom'
+import * as React from '../react-quiz/node_modules/react'
+import { render, fireEvent, screen } from '../react-quiz/node_modules/@testing-library/react'
+import App from '../react-quiz/src/App'
+// * Development end
+
+test('Question 1 is correct', () => {
+	const firstQuestion = 'What is the capital of France?'
+	render(<App />)
+	expect(screen.getByText(firstQuestion))
+})
+
+test('Answers 1 are correct', () => {
+	render(<App />)
+	expect(screen.getByText('New York'))
+	expect(screen.getByText('London'))
+	expect(screen.getByText('Paris'))
+	expect(screen.getByText('Dublin'))
+})
